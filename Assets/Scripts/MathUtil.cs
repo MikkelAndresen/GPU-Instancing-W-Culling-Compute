31a13a598a162299b7caae927e908e22d2140787
Mikkel95 <mikkel@osc.no>
2023-01-28 12:14:58 +0100

    Added AABB4 struct and some manually vectorized methods for comparing against frustum. This struct can be very useful in conjunction with NativeArray<T>.Reintepret and SliceConvert

--- Assets/Scripts/MathUtil.cs
+++ Assets/Scripts/MathUtil.cs
@@ -34,26 +34,30 @@ public static class MathUtil
 		return true;
 	}
 
+	[System.Serializable, BurstCompatible, StructLayout(LayoutKind.Sequential), BurstCompatible]
+	public struct AABB
+	{
+		public float3 Center { get; private set; }
+		public float3 Size { get; private set; }
+		public float3 Extents => Size / 2f;
+
+		public AABB(in float3 center, in float3 size)
+		{
+			Center = center;
+			Size = size;
+		}
+
 	[MethodImpl(MethodImplOptions.AggressiveInlining)]
-	public static bool IsBoundsInFrustum(NativeArray<Plane> frustumPlanes, AABB bounds)
+		public bool IsBoundsInFrustum(NativeArray<Plane> frustumPlanes)
 	{
 		for (int i = 0; i < 6; i++)
 		{
-			if (bounds.ClassifyAgainstPlane(frustumPlanes[i]) < 0)
+				if (ClassifyAgainstPlane(frustumPlanes[i]) < 0)
 				return false;
 		}
 
 		return true;
 	}
-
-	[System.Serializable, BurstCompatible, StructLayout(LayoutKind.Sequential), BurstCompatible]
-	public struct AABB
-	{
-		public AABB(float3 center, float3 size)
-		{
-			Center = center;
-			Size = size;
-		}
 
 		/// <summary>
 		/// Returns 0 if this bounds intersects the plane.
@@ -76,8 +80,129 @@ public static class MathUtil
 			return d - r;
 		}
 
-		public float3 Center { get; private set; }
-		public float3 Size { get; private set; }
+		/// <summary>
+		/// The minimum coordinate of the AABB
+		/// </summary>
+		/// <returns>The minimum coordinate of the AABB, in three dimensions.</returns>
+		public float3 Min => Center - Extents;
+
+		/// <summary>
+		/// The maximum coordinate of the AABB
+		/// </summary>
+		/// <returns>The maximum coordinate of the AABB, in three dimensions.</returns>
+		public float3 Max => Center + Extents;
+	}
+
+	/// <summary>
+	/// This is meant for manual vectorization of functions such as raycasting
+	/// </summary>
+	[System.Serializable, BurstCompatible]
+	public unsafe struct AABB4
+	{
+		public AABB x, y, z, w;
+		/// <summary>
+		/// The location of the center of the AABB
+		/// </summary>
+		public float3x4 Center => new float3x4(x.Center, y.Center, z.Center, w.Center);
+
+		/// <summary>
+		/// A 3D vector from the center of the AABB, to the corner of the AABB with maximum XYZ values
+		/// </summary>
+		public float3x4 Extents => new float3x4(x.Extents, y.Extents, z.Extents, w.Extents);
+
+		public AABB4(in AABB x, in AABB y, in AABB z, in AABB w) : this()
+		{
+			this.x = x;
+			this.y = y;
+			this.z = z;
+			this.w = w;
+		}
+
+		/// <summary>Returns the AABB element at a specified index.</summary>
+		/// Taken from float4.gen.cs line 3240
+		unsafe public AABB this[int index]
+		{
+			get
+			{
+#if ENABLE_UNITY_COLLECTIONS_CHECKS
+				if ((uint)index >= 4)
+					throw new System.ArgumentException("index must be between[0...3]");
+#endif
+				fixed (AABB4* array = &this) { return ((AABB*)array)[index]; }
+			}
+			set
+			{
+#if ENABLE_UNITY_COLLECTIONS_CHECKS
+				if ((uint)index >= 4)
+					throw new System.ArgumentException("index must be between[0...3]");
+#endif
+				fixed (AABB* array = &x) { array[index] = value; }
+			}
+		}
+
+		/// <summary>
+		/// The size of the AABB
+		/// </summary>
+		/// <returns>The size of the AABB, in three dimensions. All three dimensions must be positive.</returns>
+		public float3x4 Size => Extents * 2;
+
+		/// <summary>
+		/// The minimum coordinate of the AABB
+		/// </summary>
+		/// <returns>The minimum coordinate of the AABB, in three dimensions.</returns>
+		public float3x4 Min => Center - Extents;
+
+		/// <summary>
+		/// The maximum coordinate of the AABB
+		/// </summary>
+		/// <returns>The maximum coordinate of the AABB, in three dimensions.</returns>
+		public float3x4 Max => Center + Extents;
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public bool4 IsBoundsInFrustum(NativeArray<Plane> frustumPlanes)
+		{
+			float4x3 size = math.transpose(Size);
+			float4x3 center = math.transpose(Center);
+
+			bool4 result = true;
+			for (int i = 0; i < 6; i++)
+				result &= AABB4.ClassifyAgainstPlane(frustumPlanes[i], size, center) >= 0;
+			return result;
+		}
+
+		/// <summary>
+		/// Returns 0 if this bounds intersects the plane.
+		/// Returns a positive number if this bounds is in front of the plane or a negative when behind.
+		/// </summary>
+		/// <param name="plane"></param>
+		/// <returns></returns>
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public float4 ClassifyAgainstPlane(in Plane plane)
+		{
+			float4x3 size = math.transpose(Size);
+			float4x3 center = math.transpose(Center);
+
+			return ClassifyAgainstPlane(plane, size, center);
+		}
+
+		/// <summary>
+		/// Returns 0 if this bounds intersects the plane.
+		/// Returns a positive number if this bounds is in front of the plane or a negative when behind.
+		/// </summary>
+		/// <param name="plane"></param>
+		/// <returns></returns>
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static float4 ClassifyAgainstPlane(in Plane plane, in float4x3 size, in float4x3 center)
+		{
+			float4 r = math.abs(size.c0 * plane.normal.x) +
+						math.abs(size.c1 * plane.normal.y) +
+						math.abs(size.c2 * plane.normal.z);
+
+			float4 d = Dot4(center.c0, center.c1, center.c2, plane.normal)
+			 + plane.distance;
+
+			return math.select(math.select(d - r, d + r, d < 0.0f), 0f, math.abs(d) < r);
+		}
 	}
 
 	[System.Serializable, BurstCompatible, StructLayout(LayoutKind.Sequential), BurstCompatible]
