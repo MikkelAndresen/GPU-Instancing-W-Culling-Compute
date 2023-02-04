f944cdb7133229b9ab9a803ae6be7300ec406e2d
Mikkel95 <mikkel@osc.no>
2023-02-04 11:24:12 +0100

    Changed the copy test method to be generic and take a datagen delegate, that way we can design many tests using the same method, which is also what I did. Added tests for all true, odd true, none true and every 4 true every 6 indices.

--- Assets/Tests/JobTests.cs
+++ Assets/Tests/JobTests.cs
@@ -14,22 +14,53 @@ public class JobTests
 		public bool Validate(float element) => element > 0;
 	}
 
-	[Test]
-	public unsafe void ParallelBitIndexingJob()
+	private struct ValidateTrue : IValidator<float>
 	{
-		int halfLength = 50;
-		NativeArray<float> data = new NativeArray<float>(halfLength * 2, Allocator.Persistent);
-		for (int i = 0; i < data.Length; i++)
-			data[i] = i % 2 == 0 ? -1 : i;
+		public bool Validate(float element) => true;
+	}
+
+	private struct ValidateFalse : IValidator<float>
+	{
+		public bool Validate(float element) => false;
+	}
+
+	[Test]
+	public void TestCopyAll1Bits() => TestParallelIndexSingleCopy<ValidateTrue>((i) => i);
+	
+	[Test]
+	public void TestCopyAll0Bits() => TestParallelIndexSingleCopy<ValidateFalse>((i) => i);
+
+	[Test]
+	public void TestCopyAllOddBits() => TestParallelIndexSingleCopy<GreaterThanZeroDel>((i) => i % 2 == 0 ? -1f : 1);
+
+	[Test]
+	public void TestCopyAllBatchedBits()
+	{
+		int j = 0;
+		TestParallelIndexSingleCopy<GreaterThanZeroDel>((i) => 
+		{
+			j++;
+			if (j >= 5)
+				j = -5;
+			return j > 0 ? 1 : -1;
+		});
+	}
+
+	private static void TestParallelIndexSingleCopy<T>(Func<float, float> dataGen) where T : unmanaged, IValidator<float>
+	{
+		// We declare this as a method because we want to use it multiple times later
+		//float GetTestValue(float i) => i;//i % 2 == 0 ? -1f : i; 
+
+		NativeArray<float> src = new NativeArray<float>(100, Allocator.Persistent);
+		for (int i = 0; i < src.Length; i++)
+		{
+			src[i] = dataGen(i);
+			//Debug.Log("D:"+data[i]);
+		}
 
 		NativeArray<BitField64> bits = new NativeArray<BitField64>((int)math.ceil(100f / 64f), Allocator.Persistent);
-		ConditionIndexingJob<float, GreaterThanZeroDel>.Schedule(data, bits, out var job).Complete();
+		ConditionIndexingJob<float, T>.Schedule(src, bits, out var job).Complete();
 		//Debug.Log(Convert.ToString((long)bits[0].Value, toBase: 2));
-
-		int bitCount = 0;
-		for (int i = 0; i < bits.Length; i++)
-			bitCount += math.countbits(bits[i].Value);
-		Assert.AreEqual(halfLength, bitCount);
 
 		NativeArray<float> dstData = new NativeArray<float>(100, Allocator.Persistent);
 		NativeReference<int> counter = new NativeReference<int>(0, Allocator.Persistent);
@@ -38,30 +69,42 @@ public class JobTests
 
 		int count = counter.Value;
 		counter.Dispose();
-		Assert.AreEqual(bitCount, count);
-
 		bits.Dispose();
 
 		// We copy all the data we wish to assert because if an assertion fails
 		// we get exceptions due to native collections not being disposed.
-		float[] dataCopy = new float[data.Length];
-		data.CopyTo(dataCopy);
+		float[] srcCopy = new float[src.Length];
+		src.CopyTo(srcCopy);
 		float[] dstCopy = new float[dstData.Length];
 		dstData.CopyTo(dstCopy);
 
-		data.Dispose();
+		src.Dispose();
 		dstData.Dispose();
+		(float[] expected, int expectedLength) = GetExpected<T>(srcCopy);
 
-		Assert.AreEqual(dstCopy.Length, dataCopy.Length);
-		var comparer = new GreaterThanZeroDel();
-		for (int i = 0; i < count && comparer.Validate(dataCopy[i]); i++)
+		Assert.AreEqual(expectedLength, count, "Incorrect length");
+		Assert.AreEqual(dstCopy.Length, srcCopy.Length, "Differing length between dst and src");
+
+		for (int i = 0; i < dstData.Length; i++)
 		{
-			//Debug.Log($"Result: {dataCopy[i]}");
-
-			if (comparer.Validate(dataCopy[i]))
-				Assert.AreEqual(dataCopy[i], dstCopy[i], $"Index {i} had the wrong value after validation");
-			else
-				Assert.AreNotEqual(-1, dstCopy[i], $"Index {i} had the wrong value after correct false validation");
+			//Debug.Log($"Expected/Actual: {expected[i]}/{dstCopy[i]}");
+			Assert.AreEqual(expected[i], dstCopy[i], $"Index {i} had the wrong value");
 		}
+	}
+
+	private static (float[] arr, int expectedLength) GetExpected<T>(float[] data) where T : IValidator<float>
+	{
+		T comparer = default;
+		float[] expected = new float[100];
+		int j = 0;
+		for (int i = 0; i < expected.Length; i++)
+		{
+			if (comparer.Validate(data[i]))
+			{
+				expected[j] = data[i];
+				j++;
+			}
+		}
+		return (expected, j);
 	}
 }
